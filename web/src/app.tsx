2736326ecaed3ba11309c2f58db65e53d4656378
Nadilson <nadilson@protonmail.com>
2024-09-11 01:57:27 -0300

    Aula 2 concluída

---
+++ web/src/app.tsx
@@ -0,0 +1,12 @@
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+
+export function App() {
+  return (
+    <Dialog>
+      <Summary />
+      <CreateGoal />
+    </Dialog>
+  )
+}
