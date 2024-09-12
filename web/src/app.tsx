4c6c585d861e9d37a2f3f5510078c37eb1d780cd
Nadilson <nadilson@protonmail.com>
2024-09-12 01:46:56 -0300

    Aula 3 concluída (App finalizado)

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,11 +1,28 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 import { Summary } from './components/summary'
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  })
+
+  /* if (isLoading || !data) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="text-zinc-500 animate-spin size-10" />
+      </div>
+    )
+  } */
+
   return (
     <Dialog>
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
